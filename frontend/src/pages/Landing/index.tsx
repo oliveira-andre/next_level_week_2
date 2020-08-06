df4209bb80ecafe9d1290e9225862b2f0717d849
Andre Oliveira <desmonplays@gmail.com>
2020-08-06 00:47:56 -0400

    fix: switch name of var from secondary to secundary

---
+++ frontend/src/pages/Landing/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="proffy" />
+          <h2>sua plataforma de cursos online</h2>
+        </div>
+
+        <img className="hero-image" src={landingImg} alt="plataforma de estudos" />
+
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="study" />
+            Estudar
+          </a>
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="give classes" />
+            Dar Aulas
+          </a>
+
+          <span className="total-connections">
+            Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="purple heart" />
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Landing;
