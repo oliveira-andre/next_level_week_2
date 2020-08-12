f86d255cdd0a91c09c93fc00bde958ef3a4eba0d
Andre Oliveira <desmonplays@gmail.com>
2020-08-11 23:22:03 -0300

    feat: show teacher by html

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList() {
@@ -23,6 +25,34 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars1.githubusercontent.com/u/30729516?s=460&u=61d4c46a6bed8656ec31eb6dc147fe6b21e2212e&v=4" alt="Andre Oliveira" />
+            <div>
+              <strong>Andre Oliveira</strong>
+              <span>Quimica</span>
+            </div>
+          </header>
+
+          <p>
+            Maluco dos códigos
+            <br /><br />
+            etc etc etc
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="entrar em contato" />
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
