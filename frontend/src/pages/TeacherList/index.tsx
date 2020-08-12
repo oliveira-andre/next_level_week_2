7619e50b4458096c3ac0401460e1363f740c00b3
Andre Oliveira <desmonplays@gmail.com>
2020-08-11 22:18:20 -0300

    feat: moving header to separated component

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
+import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="voltar" />
-          </Link>
-            <img src={logoImg} alt="proffy" />
-        </div>
-        <div className="header-content">
-          <strongs>Estes são os Proffys disponíveis</strongs>
-        </div>
-      </header>
+      <PageHeader />
     </div>
   );
 }
