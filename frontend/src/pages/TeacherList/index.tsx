18801514d1b4f1710a7efedfa96a7276d5f6c6f8
Andre Oliveira <desmonplays@gmail.com>
2020-08-11 23:55:58 -0300

    feat: importin teacher item component

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -27,31 +26,7 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars1.githubusercontent.com/u/30729516?s=460&u=61d4c46a6bed8656ec31eb6dc147fe6b21e2212e&v=4" alt="Andre Oliveira" />
-            <div>
-              <strong>Andre Oliveira</strong>
-              <span>Quimica</span>
-            </div>
-          </header>
-
-          <p>
-            Maluco dos códigos
-            <br /><br />
-            etc etc etc
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="entrar em contato" />
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
       </main>
     </div>
   );
