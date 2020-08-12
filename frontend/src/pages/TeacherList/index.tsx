af23277ced8e1f74d517418bb28c348e6e15b7b8
Andre Oliveira <desmonplays@gmail.com>
2020-08-11 22:12:22 -0300

    feat: styling and generating the html of header of proffy page

--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+import './styles.css';
 
 function TeacherList() {
-  return <h1>teachers</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="voltar" />
+          </Link>
+            <img src={logoImg} alt="proffy" />
+        </div>
+        <div className="header-content">
+          <strongs>Estes são os Proffys disponíveis</strongs>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default TeacherList;
